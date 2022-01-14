70c3316cbcbd603e77d75c119d8e0f5615a87297
iceE-1591 <icee1591@gmail.com>
2022-01-14 23:04:35 +0900

    仮のFragmentファイルを二つ作成

---
+++ app/src/main/java/com/icee/snakehealthmanagementapp/Fragment1.kt
@@ -0,0 +1,21 @@
+package com.icee.snakehealthmanagementapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class Fragment1: Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment1, container, false)
+        return view
+    }
+}
