4721b7a34f8958fdc8a14a82a574d8819b9c972d
iceE-1591 <icee1591@gmail.com>
2022-01-30 17:02:48 +0900

    Main画面のParent画面遷移用メソッドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/MainActivity.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/MainActivity.kt
@@ -9,6 +9,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    fun navMain() {
+        setContentView(R.layout.activity_main)
+    }
+
     fun navLogin() {
         setContentView(R.layout.activity_login)
     }
