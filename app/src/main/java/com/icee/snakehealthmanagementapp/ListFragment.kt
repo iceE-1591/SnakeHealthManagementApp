f8c371d8fa9c5bdfb1aee0b419cc9a7ec9e2e1ef
iceE-1591 <icee1591@gmail.com>
2022-02-01 02:53:20 +0900

    ListFragmentのクリック時処理を追加

--- app/src/main/java/com/icee/snakehealthmanagementapp/ListFragment.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/ListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.AdapterView
 
 class ListFragment : ListFragment() {
     var data = listOf<String>("test1","test2")
@@ -15,5 +16,13 @@ class ListFragment : ListFragment() {
         val adapter = ItemAdapter(this.requireContext(), data)
         listAdapter = adapter
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { av, v, pos, id ->
+                System.out.println(pos)
+            }
     }
 }
