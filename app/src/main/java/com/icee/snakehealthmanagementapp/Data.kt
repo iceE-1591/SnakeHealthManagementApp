97afdb1da76c776c8dffff692034b2f682021433
iceE-1591 <icee1591@gmail.com>
2022-02-01 02:45:21 +0900

    ViewHolderクラスを作成 ListAdapter用

--- app/src/main/java/com/icee/snakehealthmanagementapp/Data.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/Data.kt
@@ -1,1 +1,9 @@
 package com.icee.snakehealthmanagementapp
+
+import android.widget.ImageView
+import android.widget.TextView
+
+data class ViewHolderItem(
+    val icon: ImageView,
+    val name: TextView
+)
