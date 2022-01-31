e3a1837b35a2ecf8bcde2989175ca4919415dd5b
iceE-1591 <icee1591@gmail.com>
2022-02-01 03:05:22 +0900

    ListFragmentのカスタムレイアウト用DataClassを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/Data.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/Data.kt
@@ -3,6 +3,11 @@ package com.icee.snakehealthmanagementapp
 import android.widget.ImageView
 import android.widget.TextView
 
+data class RowData(
+    var imagePath: String,
+    var name: String
+)
+
 data class ViewHolderItem(
     val icon: ImageView,
     val name: TextView
