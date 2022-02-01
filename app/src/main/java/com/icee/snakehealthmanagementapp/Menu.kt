23bbb23c1cccb5d6719bab0617a28708d2780585
iceE-1591 <icee1591@gmail.com>
2022-02-02 02:15:22 +0900

    Menuクリック時に背景色を変更するメソッドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/Menu.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/Menu.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class Menu: Fragment(){
+    //profile,settings,feedback,helpが入る
+    private val views = mutableListOf<View>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,6 +20,16 @@ class Menu: Fragment(){
         val settings: View = view.findViewById(R.id.settings)
         val feedback: View = view.findViewById(R.id.feedback)
         val help: View = view.findViewById(R.id.help)
+        views.addAll(listOf(profile, settings, feedback, help))
         return view
+    }
+
+    fun changeBackgroundColor(view: View) {
+        val viewsCopy = views.toMutableList()
+        viewsCopy.remove(view)
+        viewsCopy.forEach{ v ->
+            v.setBackgroundResource(R.color.MenuColor)
+        }
+        view.setBackgroundResource(R.color.MainColor)
     }
 }
