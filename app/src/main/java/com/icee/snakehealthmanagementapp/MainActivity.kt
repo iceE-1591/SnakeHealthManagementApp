a35a6ab1d386904c5457c0b164cc135d29bf34d6
iceE-1591 <icee1591@gmail.com>
2022-02-12 23:26:51 +0900

    スマートフォンの場合、縦画面固定にするように

--- app/src/main/java/com/icee/snakehealthmanagementapp/MainActivity.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.icee.snakehealthmanagementapp
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(!resources.getBoolean(R.bool.is_tablet)) // 表示機器がスマートフォンの場合は縦画面固定にする
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         navMain()
     }
 
