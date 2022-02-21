b32d60044f82b8313f2dc19ff9e8f07a60955e7c
iceE-1591 <icee1591@gmail.com>
2022-02-22 00:59:09 +0900

    fragment2のviewクラスを作成

---
+++ app/src/main/java/com/icee/snakehealthmanagementapp/view/Fragment2.kt
@@ -0,0 +1,32 @@
+package com.icee.snakehealthmanagementapp.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.icee.snakehealthmanagementapp.R
+import com.icee.snakehealthmanagementapp.databinding.Fragment2Binding
+import com.icee.snakehealthmanagementapp.viewmodel.Fragment2Data
+
+class Fragment2: Fragment(){
+    private lateinit var binding: Fragment2Binding
+    private val viewmodel by viewModels<Fragment2Data>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment2, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.vm = viewmodel
+        return binding.root
+    }
+}
