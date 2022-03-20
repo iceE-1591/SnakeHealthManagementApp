43ce6cb7f445d506797a8bcf1fb862ab6325870a
iceE-1591 <icee1591@gmail.com>
2022-03-21 00:45:13 +0900

    RegisterFragmentのViewクラスを作成

---
+++ app/src/main/java/com/icee/snakehealthmanagementapp/view/RegisterFragment.kt
@@ -0,0 +1,28 @@
+package com.icee.snakehealthmanagementapp.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.icee.snakehealthmanagementapp.R
+import com.icee.snakehealthmanagementapp.databinding.FragmentRegisterBinding
+import com.icee.snakehealthmanagementapp.viewmodel.RegisterData
+
+class RegisterFragment: Fragment(){
+    private lateinit var binding: FragmentRegisterBinding
+    private val viewmodel by viewModels<RegisterData>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.vm = viewmodel
+        return binding.root
+    }
+}
