6d758da9fb9b5c23e3df7c5ba88c7b6bb6f92f83
iceE-1591 <icee1591@gmail.com>
2022-03-30 00:59:03 +0900

    Verify画面でのClick時のメソッドとViewに通知を送るためのフィールドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
@@ -1,6 +1,17 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.hadilq.liveevent.LiveEvent
 
 class VerifyData : ViewModel() {
+    private val _clickedState = LiveEvent<String>()
+    val clickedState : LiveData<String> = _clickedState
+
+    fun toReset() {
+        _clickedState.value = "RESET"
+    }
+    fun toLogin() {
+        _clickedState.value = "LOGIN"
+    }
 }
