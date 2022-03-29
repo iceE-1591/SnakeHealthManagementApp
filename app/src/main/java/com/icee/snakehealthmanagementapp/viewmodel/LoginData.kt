1e8cf5dad3589f1719d417bdd7e787eb5248c0af
iceE-1591 <icee1591@gmail.com>
2022-03-30 00:54:43 +0900

    Login画面でのClick時のメソッドとViewに通知を送るためのフィールドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
@@ -1,6 +1,20 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.hadilq.liveevent.LiveEvent
 
 class LoginData : ViewModel() {
+    private val _clickedState = LiveEvent<String>()
+    val clickedState : LiveData<String> = _clickedState
+
+    fun toMain() {
+        _clickedState.value = "MAIN"
+    }
+    fun toRegister() {
+        _clickedState.value = "REGISTER"
+    }
+    fun toVerify() {
+        _clickedState.value = "VERIFY"
+    }
 }
