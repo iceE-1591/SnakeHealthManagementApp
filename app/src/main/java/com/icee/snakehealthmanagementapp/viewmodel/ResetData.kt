194f6e596f81b5136f80319c2950bcde481172a4
iceE-1591 <icee1591@gmail.com>
2022-03-30 00:59:47 +0900

    Reset画面でのClick時のメソッドとViewに通知を送るためのフィールドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/ResetData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/ResetData.kt
@@ -1,6 +1,17 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.hadilq.liveevent.LiveEvent
 
 class ResetData : ViewModel() {
+    private val _clickedState = LiveEvent<String>()
+    val clickedState : LiveData<String> = _clickedState
+
+    fun toMain() {
+        _clickedState.value = "MAIN"
+    }
+    fun toLogin() {
+        _clickedState.value = "LOGIN"
+    }
 }
