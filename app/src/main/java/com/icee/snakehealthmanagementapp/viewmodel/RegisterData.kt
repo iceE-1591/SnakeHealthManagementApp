bddc3c7d632f8546d4474ae0ad860b57b403c026
iceE-1591 <icee1591@gmail.com>
2022-03-31 01:29:51 +0900

    Register画面のTextInputEditText用フィールドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/RegisterData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/RegisterData.kt
@@ -1,12 +1,15 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
 
 class RegisterData: ViewModel() {
     private val _clickedState = LiveEvent<String>()
     val clickedState : LiveData<String> = _clickedState
+    private val _name = MutableLiveData<String>()
+    val name : LiveData<String> = _name
 
     fun toMain() {
         _clickedState.value = "MAIN"
