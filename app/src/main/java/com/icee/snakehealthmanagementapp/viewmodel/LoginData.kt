ed1ea805f75f7d7221b556a99826f3118113e46b
iceE-1591 <icee1591@gmail.com>
2022-03-31 01:28:39 +0900

    Login画面のTextInputEditText用フィールドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
@@ -1,12 +1,17 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
 
 class LoginData : ViewModel() {
     private val _clickedState = LiveEvent<String>()
     val clickedState : LiveData<String> = _clickedState
+    private val _email = MutableLiveData<String>()
+    val email : LiveData<String> = _email
+    private val _password = MutableLiveData<String>()
+    val password : LiveData<String> = _password
 
     fun toMain() {
         _clickedState.value = "MAIN"
