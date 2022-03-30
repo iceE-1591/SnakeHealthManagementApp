6e82da56c30a18ce7a86890fe86619633d499b87
iceE-1591 <icee1591@gmail.com>
2022-03-31 01:30:15 +0900

    Verify画面のTextInputEditText用フィールドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
@@ -1,12 +1,15 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
 
 class VerifyData : ViewModel() {
     private val _clickedState = LiveEvent<String>()
     val clickedState : LiveData<String> = _clickedState
+    private val _code = MutableLiveData<String>()
+    val code : LiveData<String> = _code
 
     fun toReset() {
         _clickedState.value = "RESET"
