cd84e2a24fb11c78e5c9364ebbaf0101106a86a2
iceE-1591 <icee1591@gmail.com>
2022-03-31 01:30:49 +0900

    Reset画面のTextInputEditText用フィールドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/ResetData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/ResetData.kt
@@ -1,12 +1,17 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
 
 class ResetData : ViewModel() {
     private val _clickedState = LiveEvent<String>()
     val clickedState : LiveData<String> = _clickedState
+    private val _password = MutableLiveData<String>()
+    val password : LiveData<String> = _password
+    private val _confirm = MutableLiveData<String>()
+    val confirm : LiveData<String> = _confirm
 
     fun toMain() {
         _clickedState.value = "MAIN"
