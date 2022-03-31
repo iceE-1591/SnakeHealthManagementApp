6d8a5f80a2a7c807a66c02dda46eda0733a3cbac
iceE-1591 <icee1591@gmail.com>
2022-03-31 13:00:02 +0900

    Reset画面のclickedStateフィールドのジェネリクスをStringからClickedStateに変更

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/ResetData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/ResetData.kt
@@ -4,19 +4,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
+import com.icee.snakehealthmanagementapp.constant.ClickedState
 
 class ResetData : ViewModel() {
-    private val _clickedState = LiveEvent<String>()
-    val clickedState : LiveData<String> = _clickedState
+    private val _clickedState = LiveEvent<ClickedState>()
+    val clickedState : LiveData<ClickedState> = _clickedState
     private val _password = MutableLiveData<String>()
     val password : LiveData<String> = _password
     private val _confirm = MutableLiveData<String>()
     val confirm : LiveData<String> = _confirm
 
     fun toMain() {
-        _clickedState.value = "MAIN"
+        _clickedState.value = ClickedState.MAIN
     }
     fun toLogin() {
-        _clickedState.value = "LOGIN"
+        _clickedState.value = ClickedState.LOGIN
     }
 }
