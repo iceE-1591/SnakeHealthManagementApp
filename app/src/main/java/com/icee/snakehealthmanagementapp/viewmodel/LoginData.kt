f9545ace8c9dfdcb8c83c530a24ec0832ddc78d7
iceE-1591 <icee1591@gmail.com>
2022-03-31 12:57:24 +0900

    Login画面のclickedStateフィールドのジェネリクスをStringからClickedStateに変更

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
@@ -4,22 +4,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
+import com.icee.snakehealthmanagementapp.constant.ClickedState
 
 class LoginData : ViewModel() {
-    private val _clickedState = LiveEvent<String>()
-    val clickedState : LiveData<String> = _clickedState
+    private val _clickedState = LiveEvent<ClickedState>()
+    val clickedState : LiveData<ClickedState> = _clickedState
     private val _email = MutableLiveData<String>()
     val email : LiveData<String> = _email
     private val _password = MutableLiveData<String>()
     val password : LiveData<String> = _password
 
     fun toMain() {
-        _clickedState.value = "MAIN"
+        _clickedState.value = ClickedState.MAIN
     }
     fun toRegister() {
-        _clickedState.value = "REGISTER"
+        _clickedState.value = ClickedState.REGISTER
     }
     fun toVerify() {
-        _clickedState.value = "VERIFY"
+        _clickedState.value = ClickedState.VERIFY
     }
 }
