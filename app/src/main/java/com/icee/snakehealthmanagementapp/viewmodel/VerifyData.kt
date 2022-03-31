2905949512948d215a16fef46b7107c18c231bbc
iceE-1591 <icee1591@gmail.com>
2022-03-31 12:59:41 +0900

    Verify画面のclickedStateフィールドのジェネリクスをStringからClickedStateに変更

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
@@ -4,17 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
+import com.icee.snakehealthmanagementapp.constant.ClickedState
 
 class VerifyData : ViewModel() {
-    private val _clickedState = LiveEvent<String>()
-    val clickedState : LiveData<String> = _clickedState
+    private val _clickedState = LiveEvent<ClickedState>()
+    val clickedState : LiveData<ClickedState> = _clickedState
     private val _code = MutableLiveData<String>()
     val code : LiveData<String> = _code
 
     fun toReset() {
-        _clickedState.value = "RESET"
+        _clickedState.value = ClickedState.RESET
     }
     fun toLogin() {
-        _clickedState.value = "LOGIN"
+        _clickedState.value = ClickedState.LOGIN
     }
 }
