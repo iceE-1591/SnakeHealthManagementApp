42f5265a4e0048105c4e694409cf41255f9fcfe5
iceE-1591 <icee1591@gmail.com>
2022-04-08 17:01:22 +0900

    Login画面のエラー表示用TextViewの双方向データバインディング用変数を配置

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
@@ -8,17 +8,20 @@ import com.icee.snakehealthmanagementapp.constant.ClickedState
 
 class LoginData : ViewModel() {
     private val _clickedState = LiveEvent<ClickedState>()
-    val clickedState : LiveData<ClickedState> = _clickedState
+    val clickedState: LiveData<ClickedState> = _clickedState
     val checkbox = MutableLiveData<Boolean>()
     val email = MutableLiveData<String>()
     val password = MutableLiveData<String>()
+    val errorText = MutableLiveData<String>()
 
     fun toMain() {
         _clickedState.value = ClickedState.MAIN
     }
+
     fun toRegister() {
         _clickedState.value = ClickedState.REGISTER
     }
+
     fun toVerify() {
         _clickedState.value = ClickedState.VERIFY
     }
