3da042056364b6b767d4ac170f8c5e525b5fce0f
iceE-1591 <icee1591@gmail.com>
2022-04-15 01:51:51 +0900

    入力されたコードが4桁じゃない場合、エラーを表示するメソッドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
@@ -5,11 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
 import com.icee.snakehealthmanagementapp.constant.ClickedState
+import com.icee.snakehealthmanagementapp.util.regex
 
 class VerifyData : ViewModel() {
     private val _clickedState = LiveEvent<ClickedState>()
     val clickedState : LiveData<ClickedState> = _clickedState
-    val code = MutableLiveData<String>()
+    val code = MutableLiveData("")
     val errorText = MutableLiveData<String>()
 
     fun toReset() {
@@ -19,6 +20,13 @@ class VerifyData : ViewModel() {
         _clickedState.value = ClickedState.LOGIN
     }
 
+    fun checkCode() : Boolean {
+        val checkedCode = code.value.toString().regex {
+            codeValidation()
+        }
+        errorText.value = checkedCode.text
+        return checkedCode.isError
+    }
     fun reSend() {
     }
 }
