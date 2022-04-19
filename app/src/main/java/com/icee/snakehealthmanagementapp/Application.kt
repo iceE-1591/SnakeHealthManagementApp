7cd44228e0200a63c151d7256084deb70c477946
iceE-1591 <icee1591@gmail.com>
2022-04-19 17:46:53 +0900

    Api.ktのinitを実行するためにApplicationクラスで呼び出し

--- app/src/main/java/com/icee/snakehealthmanagementapp/Application.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/Application.kt
@@ -1,9 +1,11 @@
 package com.icee.snakehealthmanagementapp
 
 import android.app.Application
+import com.icee.snakehealthmanagementapp.api.Api
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
+        Api
     }
 }
