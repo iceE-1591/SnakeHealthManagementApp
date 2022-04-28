d94d8a3366f4238b28498dbaba52b6c10fe135c1
iceE-1591 <icee1591@gmail.com>
2022-04-29 00:24:33 +0900

    アプリ全体の共有データを提供するためのSingletonClassを作成

---
+++ app/src/main/java/com/icee/snakehealthmanagementapp/SharedDatas.kt
@@ -0,0 +1,9 @@
+package com.icee.snakehealthmanagementapp
+
+object SharedDatas {
+    lateinit var code: String
+    lateinit var id: String
+    lateinit var name: String
+    lateinit var address: String
+    lateinit var iconPath: String
+}
