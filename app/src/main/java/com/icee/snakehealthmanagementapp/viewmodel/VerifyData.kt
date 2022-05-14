d74a01f26ddeb6f330300d26a7d4e1baba604aca
iceE-1591 <icee1591@gmail.com>
2022-05-15 03:59:01 +0900

    再送信メソッドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
@@ -1,11 +1,17 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.hadilq.liveevent.LiveEvent
+import com.icee.snakehealthmanagementapp.SharedDatas
 import com.icee.snakehealthmanagementapp.constant.ClickedState
+import com.icee.snakehealthmanagementapp.model.MailModel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
 
 class VerifyData(application: Application) : AndroidViewModel(application) {
     private val _clickedState = LiveEvent<ClickedState>()
@@ -18,5 +24,11 @@ class VerifyData(application: Application) : AndroidViewModel(application) {
     }
 
     fun reSend() {
+        viewModelScope.launch {
+            val result = withTimeoutOrNull(5000) {
+                MailModel().sendCode(SharedDatas.address)
+            }
+            result ?: Toast.makeText(getApplication(), "インターネットが接続されていない可能性があります。", Toast.LENGTH_LONG).show()
+        }
     }
 }
