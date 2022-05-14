adafdce08e29da76830ac02546c23452ee72952e
iceE-1591 <icee1591@gmail.com>
2022-05-14 17:26:08 +0900

    toResetメソッドを削除

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/VerifyData.kt
@@ -1,20 +1,18 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
 import com.icee.snakehealthmanagementapp.constant.ClickedState
 
-class VerifyData : ViewModel() {
+class VerifyData(application: Application) : AndroidViewModel(application) {
     private val _clickedState = LiveEvent<ClickedState>()
     val clickedState : LiveData<ClickedState> = _clickedState
     val code = MutableLiveData("")
     val errorText = MutableLiveData<String>()
 
-    fun toReset() {
-        _clickedState.value = ClickedState.RESET
-    }
     fun toLogin() {
         _clickedState.value = ClickedState.LOGIN
     }
