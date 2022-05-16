6c8d6e8a1d2aef5cd6ac82424ea7e7a3c1cd484a
iceE-1591 <icee1591@gmail.com>
2022-05-16 23:36:20 +0900

    API GET:user/loginを呼び出すメソッドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/repository/UserRepository.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/repository/UserRepository.kt
@@ -1,9 +1,13 @@
 package com.icee.snakehealthmanagementapp.repository
 
+import com.icee.snakehealthmanagementapp.entity.User
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface UserRepository {
     @GET("users/{address}")
     suspend fun search(@Path("address") address: String) : Boolean
+
+    @GET("users/login/{address}/{password}")
+    suspend fun login(@Path("address") address: String, @Path("password") password: String) : User
 }
