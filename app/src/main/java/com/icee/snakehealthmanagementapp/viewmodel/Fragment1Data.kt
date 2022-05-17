2ee7f08c7438d1eaaea116c167d3d40d0aa1ff52
iceE-1591 <icee1591@gmail.com>
2022-05-17 23:33:52 +0900

    テスト用の画面を削除

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/Fragment1Data.kt
+++
@@ -1,7 +0,0 @@
-package com.icee.snakehealthmanagementapp.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class Fragment1Data : ViewModel(){
-    val test : String = "test"
-}
--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/Fragment2Data.kt
+++
@@ -1,6 +0,0 @@
-package com.icee.snakehealthmanagementapp.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class Fragment2Data : ViewModel() {
-}
