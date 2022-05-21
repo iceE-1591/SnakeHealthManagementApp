5554a7a01499a401bebc556b97b3b3273fa1912f
iceE-1591 <icee1591@gmail.com>
2022-05-21 15:47:55 +0900

    Modelを呼び出すメソッドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/LoginData.kt
@@ -1,12 +1,20 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.hadilq.liveevent.LiveEvent
+import com.icee.snakehealthmanagementapp.SharedDatas
 import com.icee.snakehealthmanagementapp.constant.ClickedState
+import com.icee.snakehealthmanagementapp.entity.User
+import com.icee.snakehealthmanagementapp.model.MailModel
+import com.icee.snakehealthmanagementapp.model.UserModel
 import com.icee.snakehealthmanagementapp.util.regex
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
 
 class LoginData(application: Application) : AndroidViewModel(application) {
     private val _clickedState = LiveEvent<ClickedState>()
@@ -39,5 +47,35 @@ class LoginData(application: Application) : AndroidViewModel(application) {
         }
         errorText.value = checkedPassword.text
         return checkedPassword.isError
+    }
+
+    private fun search() : Boolean {
+        var sResult = false // アカウントが登録されていればtrue
+        viewModelScope.launch {
+            val result = withTimeoutOrNull(5000) {
+                sResult = UserModel().search(email.value.toString())
+            }
+            result ?: Toast.makeText(getApplication(), "インターネットが接続されていない可能性があります。", Toast.LENGTH_LONG).show()
+        }
+        return sResult
+    }
+    private fun login() : User {
+        var sResult = User()
+        viewModelScope.launch {
+            val result = withTimeoutOrNull(5000) {
+                sResult = UserModel().login(email.value.toString(), password.value.toString())
+            }
+            result ?: Toast.makeText(getApplication(), "インターネットが接続されていない可能性があります。", Toast.LENGTH_LONG).show()
+        }
+        return sResult
+    }
+    private fun sendCode() {
+        viewModelScope.launch {
+            val result = withTimeoutOrNull(5000) {
+                SharedDatas.address = email.value.toString()
+                MailModel().sendCode(email.value.toString())
+            }
+            result ?: Toast.makeText(getApplication(), "インターネットが接続されていない可能性があります。", Toast.LENGTH_LONG).show()
+        }
     }
 }
