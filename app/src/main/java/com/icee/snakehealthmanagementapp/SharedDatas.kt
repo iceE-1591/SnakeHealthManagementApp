17cf4cb55c24e841e3dc06b850ee9afe0668396d
iceE-1591 <icee1591@gmail.com>
2022-05-23 16:10:48 +0900

    UserLoginが完了した時などに呼び出されるデータセット用メソッドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/SharedDatas.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/SharedDatas.kt
@@ -1,9 +1,18 @@
 package com.icee.snakehealthmanagementapp
 
+import com.icee.snakehealthmanagementapp.entity.User
+
 object SharedDatas {
     var code = ""
     var id: Int = 0
     var name: String = ""
     var address: String = ""
     var iconPath: String? = null
+
+    fun setUser(user: User) {
+        id = user.id
+        name = user.name
+        address = user.address
+        iconPath = user.iconPath
+    }
 }
