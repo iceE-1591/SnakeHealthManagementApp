30193d166817e92dec8a352bb3b7eb867bd504bc
iceE-1591 <icee1591@gmail.com>
2022-05-23 15:53:49 +0900

    Userデータクラスの初期値を設定しないように変更

--- app/src/main/java/com/icee/snakehealthmanagementapp/entity/User.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/entity/User.kt
@@ -1,9 +1,9 @@
 package com.icee.snakehealthmanagementapp.entity
 
 data class User(
-    var id: Int = 0,
-    var name: String = "",
-    var address: String = "",
-    var password: String = "",
-    var iconPath: String? = null
+    var id: Int,
+    var name: String,
+    var address: String,
+    var password: String,
+    var iconPath: String?
 )
