5760124b1666c5cc0e90a35787fb17ff41430c31
iceE-1591 <icee1591@gmail.com>
2022-05-23 16:14:13 +0900

    全てのフィールドをprivateに

--- app/src/main/java/com/icee/snakehealthmanagementapp/SharedDatas.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/SharedDatas.kt
@@ -3,11 +3,11 @@ package com.icee.snakehealthmanagementapp
 import com.icee.snakehealthmanagementapp.entity.User
 
 object SharedDatas {
-    var code = ""
-    var id: Int = 0
-    var name: String = ""
-    var address: String = ""
-    var iconPath: String? = null
+    private var code = ""
+    private var id: Int = 0
+    private var name: String = ""
+    private var address: String = ""
+    private var iconPath: String? = null
 
     fun setUser(user: User) {
         id = user.id
