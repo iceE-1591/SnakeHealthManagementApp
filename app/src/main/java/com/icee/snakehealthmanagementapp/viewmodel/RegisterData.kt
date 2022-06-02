5d890b07ec577e03abcac18c9bcd5c31b0535e0f
iceE-1591 <icee1591@gmail.com>
2022-06-02 22:30:15 +0900

    ResisterDataをAndroidViewModelに変更

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/RegisterData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/RegisterData.kt
@@ -1,13 +1,14 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.hadilq.liveevent.LiveEvent
 import com.icee.snakehealthmanagementapp.constant.ClickedState
 import com.icee.snakehealthmanagementapp.util.regex
 
-class RegisterData: ViewModel() {
+class RegisterData(application: Application) : AndroidViewModel(application) {
     private val _clickedState = LiveEvent<ClickedState>()
     val clickedState : LiveData<ClickedState> = _clickedState
     val name = MutableLiveData<String>()
