912145cfcab780c2261bd890cc42ca5e564c441a
iceE-1591 <icee1591@gmail.com>
2022-06-03 23:10:45 +0900

    API POST:createを実行するためのメソッドを作成

--- app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/RegisterData.kt
+++ app/src/main/java/com/icee/snakehealthmanagementapp/viewmodel/RegisterData.kt
@@ -1,12 +1,18 @@
 package com.icee.snakehealthmanagementapp.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.hadilq.liveevent.LiveEvent
 import com.icee.snakehealthmanagementapp.constant.ClickedState
+import com.icee.snakehealthmanagementapp.entity.User
+import com.icee.snakehealthmanagementapp.model.UserModel
 import com.icee.snakehealthmanagementapp.util.regex
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
 
 class RegisterData(application: Application) : AndroidViewModel(application) {
     private val _clickedState = LiveEvent<ClickedState>()
@@ -31,5 +37,16 @@ class RegisterData(application: Application) : AndroidViewModel(application) {
         }
         errorText.value = checkedName.text
         return checkedName.isError
+    }
+
+    private fun create(user: User) : Int {
+        var sResult = 0
+        viewModelScope.launch {
+            val result = withTimeoutOrNull(5000) {
+                sResult = UserModel().create(user)
+            }
+            result ?: Toast.makeText(getApplication(), "インターネットが接続されていない可能性があります。", Toast.LENGTH_LONG).show()
+        }
+        return sResult
     }
 }
